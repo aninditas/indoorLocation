cc1663ef8eac414d53134513b92f99faf31c20d3
aninditas <aninditasuryarasmi@gmail.com>
2021-03-17 20:24:12 +0800

    X and Y are encoded

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Mon Mar 15 14:20:38 2021
 
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
@@ -21,9 +22,9 @@ def obtain_feature_data():
     feature_data = np.empty((0,8)) 
     waypoint_data = np.empty((0,6))
     for file_txt in path_filenames:
-        path = float(int(Path(file_txt).name[:-4],16))
-        floor = float(int(Path(file_txt).parent.name,16))
-        building = float(int(Path(file_txt).parent.parent.name,16))
+        path = int(Path(file_txt).name[:-4],16)
+        floor = Path(file_txt).parent.name
+        building = int(Path(file_txt).parent.parent.name,16)
         with open(file_txt, 'r', encoding='utf-8') as file:
             lines = file.readlines()
         for line_data in lines:
@@ -32,20 +33,24 @@ def obtain_feature_data():
                 continue
             line_data = line_data.split('\t')
             if line_data[1] == 'TYPE_WAYPOINT':
-                time = float(int(line_data[0],16))
+                time = int(line_data[0],16)
                 x_loc = float(line_data[2])
                 y_loc = float(line_data[3])
                 waypoint_data = np.vstack((waypoint_data,(building, path, time, x_loc, y_loc, floor)))
             if line_data[1] == 'TYPE_BEACON':
-                time = float(int(line_data[0],16))
-                uuid = float(int(line_data[2],16))
-                major = float(int(line_data[3],16))
-                minor = float(int(line_data[4],16))
-                rssi = float(int(line_data[6],16))
+                time = int(line_data[0],16)
+                uuid = int(line_data[2],16)
+                major = int(line_data[3],16)
+                minor = int(line_data[4],16)
+                rssi = int(line_data[6],16)
                 feature_data = np.vstack((feature_data,(building, path, time, uuid, major, minor, rssi, floor)))
     # add 2 dimension: x_loc and y_loc from the nearest time
+    # scaler = OrdinalEncoder()
+    # scaler.fit(feature_data[:,:-1])
+    # feature_data = scaler.transform(feature_data[:,:-1])
+    # scaler.fit(waypoint_data[:,:-3])
+    # waypoint_data = scaler.transform(waypoint_data[:,:-3])
     feature_data = pair_waypoint_data(feature_data, waypoint_data)
-    # feature_data = normalize_data(feature_data)
     return feature_data
 
 def pair_waypoint_data(feature_data, waypoint_data):
@@ -61,10 +66,19 @@ def pair_waypoint_data(feature_data, waypoint_data):
         new_feature_data = np.vstack((new_feature_data,feature_row))
     return new_feature_data
 
-def normalize_data(feature_data):
-    for col in range(feature_data.shape[1]):
-        feature_data[:,col]=feature_data[:,col]/np.max(feature_data[:,col])
-    return feature_data
+def encode_data():
+    scalerX = OrdinalEncoder()
+    scalerX.fit(x)
+    scaled_x = scalerX.transform(x)
+    
+    scalerY = OrdinalEncoder()
+    y_floor = y[:,0].reshape(-1,1)
+    scalerY.fit(y_floor)
+    scaled_y = scalerY.transform(y_floor)
+    scaled_y = np.hstack((scaled_y,y[:,-2:].astype(float)))
+    
+    return scaled_x, scaled_y
+    
 
 def split_train_val_test(x, y, val_size = 0.1, test_size=0.1):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
@@ -77,22 +91,23 @@ def split_x_y():
     print(x.shape, y.shape)
     return x,y
 
-# def build_model():
-#     model = Sequential()
-#     model.add(Dense(12, input_dim=7, activation='relu'))
-#     model.add(Dense(8, activation='relu'))
-#     model.add(Dense(3, activation='sigmoid'))
-#     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-#     return model
+def build_model():
+    model = Sequential()
+    model.add(Dense(12, input_dim=7, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(3, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
 if __name__ == '__main__':
     base_path = 'D:/Dropbox/PhD/python/IndoorLocation/IndoorLocation/'
-    dataset_path = base_path+'dataset/train/'
+    dataset_path = base_path+'dataset/sample/'
     path_filenames=load_path_data()
     feature_data = obtain_feature_data()
     x,y = split_x_y()
+    x,y = encode_data()
     x_train, y_train, x_val, y_val, x_test, y_test = split_train_val_test(x, y)
     
-    # model = build_model()
-    # model.fit(x_train,y_train, validation_data=(x_val,y_val), epochs=100, batch_size=128)
+    model = build_model()
+    model.fit(x_train,y_train, validation_data=(x_val,y_val), epochs=100, batch_size=128)
     
