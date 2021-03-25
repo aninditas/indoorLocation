0d4458b356117362e06ee7a1798621b73b1f52ae
aninditas <aninditasuryarasmi@gmail.com>
2021-03-25 23:28:08 +0800

    use the shortest timegap

--- main.py
+++ main.py
@@ -23,171 +23,119 @@ def load_path_data(dataset_path,extension):
     path_filenames = list(Path(dataset_path).glob("**/*"+extension))
     return path_filenames
 
-def obtain_feature_data(path_filenames, train=True):
-    # 8 dimension: building, path, time, uuid, major, minor, rssi, floor   
-    dimension = (11 if train==True else 10)
-    feature_data = np.empty((0,dimension)) 
-    waypoint_data = np.empty((0,6))
-    for file_txt in path_filenames:
-        path = Path(file_txt).name[:-4]
-        floor = Path(file_txt).parent.name
-        with open(file_txt, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-        for line_data in lines:
-            line_data = line_data.strip()
-
-            line_data = line_data.split('\t')
-            if line_data[1][:6] == 'SiteID':
-                building = line_data[1][7:]
-            if line_data[1] == 'TYPE_WAYPOINT':
-                time = line_data[0]
-                x_loc = line_data[2]
-                y_loc = line_data[3]
-                waypoint_data = np.vstack((waypoint_data,(building, path, time, x_loc, y_loc, floor)))
-            if line_data[1] == 'TYPE_BEACON':
-                time = line_data[0]
-                uuid = line_data[2]
-                major = line_data[3]
-                minor = line_data[4]
-                val1 = line_data[5]
-                val2 = line_data[6]
-                val3 = line_data[7]
-                val4 = line_data[8]
-                if train:
-                    feature_data = np.vstack((feature_data,(building, path, time, uuid, major, minor, val1, val2, val3, val4, floor)))
-                else:
-                    feature_data = np.vstack((feature_data,(building, path, time, uuid, major, minor, val1, val2, val3, val4)))
-        np.savetxt(base_path+"dataset/train_npy/"+building+"_"+floor+"_"+path+"_"+"f.csv", feature_data, delimiter=",", fmt='%s')
-        np.savetxt(base_path+"dataset/train_npy/"+building+"_"+floor+"_"+path+"_"+"w.csv", waypoint_data, delimiter=",", fmt='%s')
-        print("Done... "+building+"_"+floor+"_"+path)
-        feature_data = np.empty((0,dimension)) 
-        waypoint_data = np.empty((0,6))
-    # if train:
-    #     # add 2 dimension: x_loc and y_loc from the nearest time
-    #     feature_data = pair_waypoint_data(feature_data, waypoint_data)
-    # return feature_data
-
-def join_data(path_filenames, train=True):
-   # 8 dimension: building, path, time, uuid, major, minor, rssi, floor   
-    dimension = (11 if train==True else 10)
-    feature_data = np.empty((0,dimension)) 
-    waypoint_data = np.empty((0,6))
-    for file_npy in path_filenames:
-        try:
-            df = read_csv(file_npy)
-            temp = df.to_numpy()
-            if Path(file_npy).name[-5]=='f':
-                feature_data = np.vstack((feature_data,temp))
-            else:
-                waypoint_data = np.vstack((waypoint_data,temp))
-            print("Saved to file "+Path(file_npy).name)
-        except:
-            continue
-       
-    return feature_data, waypoint_data
-        
-def pair_waypoint_data(feature_data, waypoint_data):
-    new_feature_data = np.empty((0,13)) 
-    for idx, feature_row in enumerate(feature_data):
-        path = waypoint_data[:,1]==feature_row[1]
-        floor = waypoint_data[:,-1]==feature_row[-1]
-        building = waypoint_data[:,0]==feature_row[0]
-        masked_waypoint = waypoint_data[(building)&(floor)&(path)]
-        distance_waypoint = abs(masked_waypoint[:,2]-feature_row[2])
-        try:
-            shortest_distance = np.argmin(distance_waypoint)
-            feature_row = np.append(feature_row,masked_waypoint[shortest_distance,3:5])
-            new_feature_data = np.vstack((new_feature_data,feature_row))
-        except:
-            continue
-        print(idx)
-    return new_feature_data
-
-def encode_floor(y):
-    scaler = OrdinalEncoder()
-    y_floor = y[:,0].reshape(-1,1)
-    scaler.fit(y_floor)
-    return scaler, scaler.transform(y_floor)
+def obtain_feature_data(path_filenames):
+    feature_data = np.empty((0,336)) 
+    for file_csv in path_filenames:
+        df = read_csv(file_csv)
+        temp = df.to_numpy()
+        feature_data = np.vstack((feature_data, temp))
+    return feature_data    
 
 def split_train_val_test(x, y, val_size = 0.1, test_size=0.1):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
     x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1)
     return x_train, y_train, x_val, y_val, x_test, y_test 
 
-def split_x_y(feature_data):
-    x = feature_data[:,:-3].astype(float)
-    y_loc = feature_data[:,-2:].astype(float)
-    scaler, y_floor = encode_floor(feature_data[:,-3:])
-    y = np.hstack((y_floor,y_loc))
-    print(x.shape, y.shape)
-    return scaler,x,y
-
-# def build_model():
-#     model = Sequential()
-#     model.add(Dense(12, input_dim=10, activation='relu'))
-#     model.add(Dense(8, activation='relu'))
-#     model.add(Dense(3, activation='sigmoid')) 
-#     opt = tf.keras.optimizers.RMSprop(learning_rate=0.001)
-#     model.compile(loss='mean_squared_error', optimizer=opt, metrics=['accuracy'])
-#     return model
-
 def build_model():
-    inputs = Input(shape=(10,))
-    dense1 = Dense(12, activation='relu')(inputs)
-    dense2 = Dense(8, activation='relu')(dense1)
-    output_floor = Dense(floor_number, activation='softmax')(dense2)
-    output_loc = Dense(2, activation='linear')(dense2)
-    model = Model(inputs=inputs, outputs=(output_floor,output_loc))
-    opt = tf.keras.optimizers.RMSprop(learning_rate=0.001)
+    model = Sequential() 
+    model.add(Dense(8, input_dim=feature_number, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    # model.add(Dense(32, activation='relu'))
+    # model.add(Dense(32, activation='relu'))
+    # model.add(Dense(32, activation='relu'))
+    model.add(Dense(2, activation='linear')) 
+    opt = tf.keras.optimizers.SGD(learning_rate=0.001)
     model.compile(loss='mean_squared_error', optimizer=opt, metrics=['accuracy'])
-    model.summary()
-    # plot_model(model)
     return model
 
-def preprocessing():
-    # obtain TYPE_BEACON data from text, save to csv file each path
-    data_train_path = base_path+'dataset/train/'
-    path_train_filenames = load_path_data(data_train_path,'.txt')
-    obtain_feature_data(path_train_filenames)
-    
-    # join all csv files into one file
-    data_npy_path = base_path+'dataset/train_npy/'
-    path_npy_filenames = load_path_data(data_npy_path,'.csv')
-    feature_data, waypoint_data = join_data(path_npy_filenames, train=True)
-    
-    # save the joined file
-    np.savetxt(base_path+"dataset/train_npy/feature_data_npy.csv", feature_data, delimiter=",", fmt='%s')
-    np.savetxt(base_path+"dataset/train_npy/waypoint_data_npy.csv", waypoint_data, delimiter=",", fmt='%s')
-    
-    feature_data, waypoint_data = convert_hex_to_int(feature_data, waypoint_data)
-    
-    # pair the feature_data with waypoint_data
-    paired_data = pair_waypoint_data(feature_data, waypoint_data)
-    np.savetxt(base_path+"dataset/train_npy/paired_data_npy.csv", paired_data, delimiter=",", fmt='%s')
-    
-    
-def convert_hex_to_int(feature_data, waypoint_data):
-    idx = [0,1,3,4,5,9]
+
+# def build_model():
+#     inputs = Input(shape=(10,))
+#     dense1 = Dense(12, activation='relu')(inputs)
+#     dense2 = Dense(8, activation='relu')(dense1)
+#     output_floor = Dense(floor_number, activation='softmax')(dense2)
+#     output_loc = Dense(2, activation='linear')(dense2)
+#     model = Model(inputs=inputs, outputs=(output_floor,output_loc))
+#     opt = tf.keras.optimizers.RMSprop(learning_rate=0.001)
+#     model.compile(loss='mean_squared_error', optimizer=opt, metrics=['accuracy'])
+#     model.summary()
+#     # plot_model(model)
+#     return model
+
+def convert_hex_to_int(data):
+    idx = [0,2,5]
     for idxx in idx:
-        feature_data[:,idxx]=[int(value, 16) for value in feature_data[:,idxx]]
-    idx = [0,1]
-    for idxx in idx:
-        waypoint_data[:,idxx]=[int(value, 16) for value in waypoint_data[:,idxx]]
-    return feature_data, waypoint_data
+        try:
+            data[:,idxx]=[int(value, 16) for value in data[:,idxx]]
+        except:
+            continue
+    return data
+
+def select_best_feature():
+    distance_location = np.arange(7,306,3)
+    distance = feature_data[:,distance_location]
+    min_distance = np.argmin(distance, axis=1)
+    feature_min_time = np.empty((0,3))
+    for idx,row in enumerate(feature_data):
+        feature_min_time = np.vstack((feature_min_time,row[distance_location[min_distance[idx]]-2:distance_location[min_distance[idx]]+1]))
+    feature_min_time = np.hstack((feature_data[:,0:5],feature_min_time))
+    feature_clear = feature_min_time[feature_min_time[:,7]<3000]
+    feature_int = convert_hex_to_int(feature_clear)
+    return feature_int
+
+def split_x_y():
+    x = feature_data[:,[0,2,5,6,7]].astype(float)
+    y = feature_data[:,[1,3,4]]
+    return x,y.astype(float)
+
+# def encode_x():
+#     scalers = []
+#     idx = [0,1,2]
+#     for idxx in idx:
+#         scalers.append(OrdinalEncoder())
+#         temp = x[:,idxx].reshape(-1,1)
+#         scalers[idxx].fit(temp)
+#         temp = scalers[idxx].transform(temp)
+#         x[:,idxx] = temp.squeeze()
+#     return scalers, x.astype(float)
+    
+
+def encode_x():
+    max_values=[]
+    for col in range(x.shape[1]):
+        max_values.append(np.max(np.absolute(x[:,col])))
+        x[:,col] = x[:,col]/max_values[col]
+    return max_values, np.absolute(x)                       
 
 if __name__ == '__main__':
     base_path = 'D:/Dropbox/PhD/python/IndoorLocation/IndoorLocation/'
     
+    # # create data, save to csv
+    # dataset_path = base_path+'dataset/train_kouki/'
+    # path_filenames = load_path_data(dataset_path,'.csv')
+    # feature_data = obtain_feature_data(path_filenames)
     
-    # scaler,x,y = split_x_y(feature_train)
-    # floor_number = np.max(y[:,0])+1
+    # feature_data = feature_data[:,1:306]
+    # feature_data = select_best_feature()
+    # np.savetxt(base_path+"dataset/train_kouki/feature_data.csv", feature_data, delimiter=",", fmt='%s')
     
-    # x_train, y_train, x_val, y_val, x_test, y_test = split_train_val_test(x, y)
+    # load data from csv
+    df = read_csv(base_path+"dataset/train_kouki/feature_data.csv")
+    feature_data = df.to_numpy()
     
-    # model = build_model()
-    # model.fit(x_train,(y_train[:,0],y_train[:,1:]), validation_data=(x_val,(y_val[:,0],y_val[:,1:])), epochs=10, batch_size=128)
-    # print('evaluate')
-    # model.evaluate(x_test, (y_test[:,0],y_test[:,1:]))
+    x,y = split_x_y()
+    y[:,0] = y[:,0]+2
+    scalers, x = encode_x()
+    
+    feature_number = x.shape[1]
+    floor_number = int(np.max(y[:,0])+1)
+    
+    x_train, y_train, x_val, y_val, x_test, y_test = split_train_val_test(x, y)
+    
+    model = build_model()
+    model.fit(x_train,y_train[:,1:], validation_data=(x_val,y_val[:,1:]), epochs=10, batch_size=32)
+    print('evaluate')
+    model.evaluate(x_test, (y_test[:,0],y_test[:,1:]))
     # model.save('model/'+time.strftime("%Y%m%d-%H%M%S")+'.h5')
     
     # data_test_path = base_path+'dataset/test/'
